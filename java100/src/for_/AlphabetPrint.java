6bb71d7da47d7b09af2a0620f6ef56b38ff861f6
bhinAn <nicebin34@gmail.com>
2023-07-13 18:52:30 +0900

    dd

---
+++ java100/src/for_/AlphabetPrint.java
@@ -0,0 +1,14 @@
+package for_;
+
+public class AlphabetPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// for 반복문 --> A(65) ~ Z(90)
+		for ( int i=65; i <= 90; i++ ) {
+			System.out.print( (char)i + " " );  // A B C D E F .............................. Z 
+		}
+		System.out.println();
+	}
+
+}
